98501a704939a9574f825a6a0ed91a708b1ab635
Hudson Retonde <hudsonsretonde@gmail.com>
2023-06-12 19:59:25 -0300

    Estabelecendo a lógica das rodadas de frases app1

--- projetos/app1/src/app/painel/painel.component.ts
+++ projetos/app1/src/app/painel/painel.component.ts
@@ -12,10 +12,20 @@ export class PainelComponent {
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase:'
   public resposta: string = ''
-  constructor(){console.log(this.frases)}
+
+  public rodada: number = 0
+  public rodadaFrase: Frase 
+
+  constructor(){
+    this.rodadaFrase = this.frases[this.rodada]
+    console.log(this.frases)
+  }
 
   public atualizaResposta(resposta: Event):void {
     this.resposta = (<HTMLInputElement>resposta.target).value
-    console.log(this.resposta)
+    //console.log(this.resposta)
+  }
+  public verificarResposta():void{
+    console.log('Verifica resposta: ', this.resposta)
   }
 }
